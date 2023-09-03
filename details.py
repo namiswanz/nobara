b63895e9251ea521b1533109a13c961ad6e3aefd
namiswanz <143374910+namiswanz@users.noreply.github.com>
2023-09-03 23:47:43 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,3 @@
-api_id = 10577960
-api_hash = "80fd047285f4e94ca80311928b6bb5da"
-bot_token = "6150468285:AAGIG6MiFTHpeOpUf7YZWssgODqH3rlc_2k"
-
+api_id = 9541960
+api_hash = "17a1aa509b35564481dae98d89d0191d"
+bot_token = "6610143533:AAFvYZMQtcJeFenFlOAVTRP1pMo8F_f47HI"
